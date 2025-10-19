eefa029b91f95cdbffb173aa50ca769ad607822d
Niek Stevenson <niek.stevenson@gmail.com>
2025-10-19 21:55:44 +0200

    clean up

--- run_profiles_new.R
+++ run_profiles_new.R
@@ -7,7 +7,7 @@ source("R/profile_plot_new.R")
 source("R/processing_tree.R")
 
 set.seed(123456)
-model_spec <- new_api_examples[[2]]
+model_spec <- new_api_examples[[6]]
 
 # Translate model specification to table representation
 model_tables <- model_to_tables(model_spec)
@@ -31,14 +31,19 @@ print(table(data$outcome, useNA = "ifany")/nrow(data))
 
 #
 # 3) Analytic probability check via likelihood helpers
-cat("\nAnalytic outcome probabilities:\n")
-probs <- observed_response_probabilities(model_tables, include_na = TRUE)
-print(round(probs, 6))
-cat(sprintf("Sum of probabilities: %.6f\n", sum(probs)))
+# cat("\nAnalytic outcome probabilities:\n")
+# probs <- observed_response_probabilities(model_tables, include_na = TRUE)
+# print(round(probs, 6))
+# cat(sprintf("Sum of probabilities: %.6f\n", sum(probs)))
 #
 # # #
 # 4) Ensure likelihood is finite on provided data
-ll <- compute_loglik(model_tables, data)
+library(profvis)
+profvis(
+  ll <- compute_loglik(model_tables, data)
+)
+
+
 cat(sprintf("\nLog-likelihood on test data: %.4f\n", ll))
 #
 # profile_res <- profile_likelihood(

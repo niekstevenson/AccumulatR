a7cd4ca25ce643b0c8ec23cec8a9bf1b098a10e7
Niek Stevenson <niek.stevenson@gmail.com>
2025-11-03 14:59:57 +0100

    updated speed profile test

--- speed_profile.R
+++ speed_profile.R
@@ -5,7 +5,11 @@ source("R/generator_new.R")
 source("R/super_large_likelihood.R")
 
 set.seed(123456)
-model_spec <- new_api_examples[[2]]
+
+# Example 1 ---------------------------------------------------------------
+
+
+model_spec <- new_api_examples[[1]]
 
 # Translate model specification to table representation
 model_tables <- model_to_tables(model_spec)
@@ -16,12 +20,12 @@ sum(ll)
 library(profvis)
 profvis({
   ll <- compute_loglik(model_tables, data)
-}, prof_output = "ll_new.out"
+}, prof_output = "profiles/ll_new.out"
 
 )
-df_new <- summaryRprof("ll_new.out")
-write.csv(df_new$by.total, file = "ll_new_total.csv")
-write.csv(df_new$by.self, file = "ll_new_self.csv")
+df_new <- summaryRprof("profiles/ll_new.out")
+write.csv(df_new$by.total, file = "profiles/ll_new_total1.csv")
+write.csv(df_new$by.self, file = "profiles/ll_new_self1.csv")
 
 # Old likelihood
 source("R/likelihood_old.R")
@@ -31,11 +35,91 @@ sum(ll)
 library(profvis)
 profvis({
   ll <- compute_loglik(model_tables, data)
-}, prof_output = "ll_old.out"
+}, prof_output = "profiles/ll_old.out"
 
 )
-df_old <- summaryRprof("ll_old.out")
-write.csv(df_old$by.total, file = "ll_old_total.csv")
-write.csv(df_old$by.self, file = "ll_old_self.csv")
+df_old <- summaryRprof("profiles/ll_old.out")
+write.csv(df_old$by.total, file = "profiles/ll_old_total1.csv")
+write.csv(df_old$by.self, file = "profiles/ll_old_self1.csv")
 
+
+# Example 2 ---------------------------------------------------------------
+rm(list = ls())
+source("examples/stim_selective_versions.R")
+source("R/model_tables.R")
+source("R/generator_new.R")
+source("R/super_large_likelihood.R")
+model_spec <- new_api_examples[[2]]
+
+# Translate model specification to table representation
+model_tables <- model_to_tables(model_spec)
+
+data <- simulate_model(model_tables, n_trials = 500)
+ll <- compute_loglik(model_tables, data)
+sum(ll)
+library(profvis)
+profvis({
+  ll <- compute_loglik(model_tables, data)
+}, prof_output = "profiles/ll_new.out"
+
+)
+df_new <- summaryRprof("profiles/ll_new.out")
+write.csv(df_new$by.total, file = "profiles/ll_new_total2.csv")
+write.csv(df_new$by.self, file = "profiles/ll_new_self2.csv")
+
+# Old likelihood
+source("R/likelihood_old.R")
+ll <- compute_loglik(model_tables, data)
+sum(ll)
+
+library(profvis)
+profvis({
+  ll <- compute_loglik(model_tables, data)
+}, prof_output = "profiles/ll_old.out"
+
+)
+df_old <- summaryRprof("profiles/ll_old.out")
+write.csv(df_old$by.total, file = "profiles/ll_old_total2.csv")
+write.csv(df_old$by.self, file = "profiles/ll_old_self2.csv")
+
+
+# Example 12 --------------------------------------------------------------
+rm(list = ls())
+source("examples/stim_selective_versions.R")
+source("R/model_tables.R")
+source("R/generator_new.R")
+source("R/super_large_likelihood.R")
+
+model_spec <- new_api_examples[[12]]
+
+# Translate model specification to table representation
+model_tables <- model_to_tables(model_spec)
+
+data <- simulate_model(model_tables, n_trials = 50)
+ll <- compute_loglik(model_tables, data)
+sum(ll)
+library(profvis)
+profvis({
+  ll <- compute_loglik(model_tables, data)
+}, prof_output = "profiles/ll_new.out"
+
+)
+df_new <- summaryRprof("profiles/ll_new.out")
+write.csv(df_new$by.total, file = "profiles/ll_new_total12.csv")
+write.csv(df_new$by.self, file = "profiles/ll_new_self12.csv")
+
+# Old likelihood
+source("R/likelihood_old.R")
+ll <- compute_loglik(model_tables, data)
+sum(ll)
+
+library(profvis)
+profvis({
+  ll <- compute_loglik(model_tables, data)
+}, prof_output = "profiles/ll_old.out"
+
+)
+df_old <- summaryRprof("profiles/ll_old.out")
+write.csv(df_old$by.total, file = "profiles/ll_old_total12.csv")
+write.csv(df_old$by.self, file = "profiles/ll_old_self12.csv")
 

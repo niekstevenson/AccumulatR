f92ebb6c067a4487c66a5513853ec00a01408465
Niek Stevenson <niek.stevenson@gmail.com>
2025-11-05 21:18:13 +0100

    made stable fast path example

--- speed_profile.R
+++ speed_profile.R
@@ -49,6 +49,8 @@ source("examples/stim_selective_versions.R")
 source("R/model_tables.R")
 source("R/generator_new.R")
 source("R/super_large_likelihood.R")
+set.seed(123456)
+
 model_spec <- new_api_examples[[2]]
 
 # Translate model specification to table representation
@@ -83,12 +85,56 @@ write.csv(df_old$by.total, file = "profiles/ll_old_total2.csv")
 write.csv(df_old$by.self, file = "profiles/ll_old_self2.csv")
 
 
+# Example 6 ---------------------------------------------------------------
+rm(list = ls())
+source("examples/stim_selective_versions.R")
+source("R/model_tables.R")
+source("R/generator_new.R")
+source("R/super_large_likelihood.R")
+set.seed(123456)
+
+model_spec <- new_api_examples[[6]]
+
+# Translate model specification to table representation
+model_tables <- model_to_tables(model_spec)
+
+data <- simulate_model(model_tables, n_trials = 500)
+ll <- compute_loglik(model_tables, data)
+sum(ll)
+library(profvis)
+profvis({
+  ll <- compute_loglik(model_tables, data)
+}, prof_output = "profiles/ll_new.out"
+
+)
+df_new <- summaryRprof("profiles/ll_new.out")
+write.csv(df_new$by.total, file = "profiles/ll_new_total6.csv")
+write.csv(df_new$by.self, file = "profiles/ll_new_self6.csv")
+
+# Old likelihood
+source("R/likelihood_old.R")
+ll <- compute_loglik(model_tables, data)
+sum(ll)
+
+library(profvis)
+profvis({
+  ll <- compute_loglik(model_tables, data)
+}, prof_output = "profiles/ll_old.out"
+
+)
+df_old <- summaryRprof("profiles/ll_old.out")
+write.csv(df_old$by.total, file = "profiles/ll_old_total6.csv")
+write.csv(df_old$by.self, file = "profiles/ll_old_self6.csv")
+
+
+
 # Example 12 --------------------------------------------------------------
 rm(list = ls())
 source("examples/stim_selective_versions.R")
 source("R/model_tables.R")
 source("R/generator_new.R")
 source("R/super_large_likelihood.R")
+set.seed(123456)
 
 model_spec <- new_api_examples[[12]]
 

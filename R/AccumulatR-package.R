82fd82a966552d2fb06843028e7c65ba8dbe5cae
Niek Stevenson <niek.stevenson@gmail.com>
2025-12-16 01:30:14 +0100

    made TBB RcppParallel work (at least for MacOS)

--- R/AccumulatR-package.R
+++ R/AccumulatR-package.R
@@ -5,6 +5,7 @@
 #' @importFrom Rcpp sourceCpp
 #' @importFrom stats setNames
 #' @importFrom utils modifyList
+#' @import RcppParallel
 #' @useDynLib AccumulatR, .registration = TRUE
 ## usethis namespace: end
 NULL
